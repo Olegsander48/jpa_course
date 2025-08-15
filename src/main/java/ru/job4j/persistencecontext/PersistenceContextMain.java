506591233820e62e43d4bf62f5656506816ade92
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-08-15 11:22:35 +0300

    persistence context flush method

--- src/main/java/ru/job4j/persistencecontext/PersistenceContextMain.java
+++ src/main/java/ru/job4j/persistencecontext/PersistenceContextMain.java
@@ -12,22 +12,28 @@ public class PersistenceContextMain {
         try {
             transaction.begin();
 
-            Teacher teacher1 = new Teacher("Natalia", "Balunova", "Math", false);
+/*            Teacher teacher1 = new Teacher("Natalia", "Balunova", "Math", false);
             Teacher teacher2 = new Teacher("Elena", "Temnikova", "English", false);
+            Teacher teacher3 = new Teacher("Vladimir", "Eroshin", "CS", true);
 
             entityManager.persist(teacher1);
             entityManager.persist(teacher2);
+            entityManager.persist(teacher3);*/
 
-            teacher1.setSubject("Physic"); //all in 1 query-update
+            Teacher teacher1 = entityManager.find(Teacher.class, 1);
+            Teacher teacher2 = entityManager.find(Teacher.class, 2);
+            Teacher teacher3 = entityManager.find(Teacher.class, 3);
+
             teacher1.setProfessor(true);
+            entityManager.remove(teacher2);
+            entityManager.flush();
+
+            Teacher teacher4 = entityManager.find(Teacher.class, 1000);
+            teacher4.getName(); //after rollback we will see no changes
+
+            teacher3.setSubject("AI");
 
             transaction.commit();
-            transaction.begin();
-            //empty transaction load all differences to DB
-            transaction.commit();
-
-            System.out.println(entityManager.find(Teacher.class, teacher1.getId()));
-            System.out.println(entityManager.find(Teacher.class, teacher1.getId()));
         } catch (Exception ex) {
             if (transaction.isActive()) {
                 transaction.rollback();
