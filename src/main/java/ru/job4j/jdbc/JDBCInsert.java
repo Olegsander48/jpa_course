6717c4fc53813552f4a889e243707ff60609a7c8
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-08-20 16:36:02 +0300

    get index from database

--- src/main/java/ru/job4j/jdbc/JDBCInsert.java
+++ src/main/java/ru/job4j/jdbc/JDBCInsert.java
@@ -10,15 +10,27 @@ public class JDBCInsert {
 
     public static void main(String[] args) {
         try(Connection connection = DriverManager.getConnection(dbUrl, user, password)) {
-            Student student = new Student("Vladimir", "Haff", 5.5);
+            Student student = new Student("Glev", "Newwel", 8.8);
 
-            Statement statement = connection.createStatement();
-            String sqlQuery = "INSERT INTO students(name, surname, avg_grade) VALUES " +
-                    "('" + student.getName() + "', '" +
-                    student.getSurname() +  "', " +
-                    student.getAvgGrade() + ");";
-            statement.executeUpdate(sqlQuery);
+            PreparedStatement statement = connection.prepareStatement(
+                    "insert into students(name, surname, avg_grade) values(?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, student.getName());
+            statement.setString(2, student.getSurname());
+            statement.setDouble(3, student.getAvgGrade());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new SQLException("Insert failed");
+            }
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                student.setId(generatedKeys.getInt(1)); //first column for indexes
+            } else {
+                throw new SQLException("Failed to get the generated id");
+            }
             statement.close();
+            generatedKeys.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
