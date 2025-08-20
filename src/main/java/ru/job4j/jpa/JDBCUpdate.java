da1be301ae1566511bafd4f1854d67cc78f2cb34
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-08-20 16:00:36 +0300

    update and sql injection

---
+++ src/main/java/ru/job4j/jpa/JDBCUpdate.java
@@ -0,0 +1,27 @@
+package ru.job4j.jpa;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class JDBCUpdate {
+    static String dbUrl = "jdbc:postgresql://localhost:5432/jpa";
+    static String user = "postgres";
+    static String password = "password";
+
+    public static void main(String[] args) {
+        try(Connection connection = DriverManager.getConnection(dbUrl, user, password)) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter student name: ");
+            String enteredName = scanner.nextLine();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE students SET avg_grade = 7.5 WHERE name = ?"
+            );
+            preparedStatement.setString(1, enteredName);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
