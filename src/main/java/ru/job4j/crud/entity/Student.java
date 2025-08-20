d685b56df9a68cc44442ab0e3c2dfa6b97a156bf
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-08-20 11:39:57 +0300

    @Transient + constraints

--- src/main/java/ru/job4j/crud/entity/Student.java
+++ src/main/java/ru/job4j/crud/entity/Student.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "students")
 @Data
@@ -13,14 +15,19 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(nullable = false, unique = true)
     private String name;
     private String surname;
     @Column(name = "avg_grade")
     private double avgGrade;
 
+    @Transient
+    private LocalDateTime createdDate;
+
     public Student(String name, String surname, double avgGrade) {
         this.name = name;
         this.surname = surname;
         this.avgGrade = avgGrade;
+        createdDate = LocalDateTime.now();
     }
 }
