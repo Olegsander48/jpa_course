b4bda37fa4970fcb815ed396f9ff4209ba79aedc
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-08-21 16:42:06 +0300

    select using hibernate + update value

---
+++ src/main/java/ru/job4j/hibernate/HibernateSelectMain.java
@@ -0,0 +1,32 @@
+package ru.job4j.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import ru.job4j.hibernate.entity.Student;
+
+public class HibernateSelectMain {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        try {
+            transaction.begin();
+
+            Student student = session.get(Student.class, 1);
+            student.setAvgGrade(10.0); //change value after commit
+            System.out.println(student);
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+    }
+}
