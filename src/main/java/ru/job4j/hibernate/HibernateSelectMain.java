c2c81c07bd5883bcdd64517819e790b894ecba79
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-09-02 09:55:46 +0300

    example of using flush with transaction rollback

--- src/main/java/ru/job4j/hibernate/HibernateSelectMain.java
+++ src/main/java/ru/job4j/hibernate/HibernateSelectMain.java
@@ -16,11 +16,16 @@ public class HibernateSelectMain {
         try {
             transaction.begin();
 
-            Student student = session.get(Student.class, 1);
+            Student student = session.get(Student.class, 6);
+            System.out.println(student); //old data
             student.setAvgGrade(10.0); //change value after commit
-            System.out.println(student);
+            System.out.println(student); //new data locally
+            session.flush(); //load data to DB
+            session.refresh(student); //load new data from DB
+            Thread.sleep(20000);
+            System.out.println(student); //get updated student
 
-            transaction.commit();
+            transaction.rollback(); //after this in db nothing changed
         } catch (Exception e) {
             transaction.rollback();
             e.printStackTrace();
